c75ff5697f1602bd61a0358300650b06ce2fa68f
Hank Feild <hank@feild.org>
2025-02-03 11:06:43 -0500

    Added/updated python scripts to use argparse, etc.

--- scripts/apache2cols.py
+++ scripts/apache2cols.py
@@ -12,6 +12,7 @@ import csv
 import sys
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL) 
+import argparse
 
 # # This assumes that certain fields will always contain a fixed number of
 # # space-separated fields.  This is not a general solution.
@@ -30,12 +31,43 @@ signal(SIGPIPE,SIG_DFL)
 #     ]
 #     print('\t'.join(logicalCols))
 
+# for line in sys.stdin:
+#     columns = csv.reader([line], delimiter=" ").__next__()
+#     print('\t'.join(columns))
 
-for line in sys.stdin:
-    columns = csv.reader([line], delimiter=" ").__next__()
-    print('\t'.join(columns))
+def formatFile(input):
+
+    for line in input:
+        columns = csv.reader([line], delimiter=" ").__next__()
+        print('\t'.join(columns))
+
+def main():
+    # Using sys.argv directly.
+    # if len(sys.argv) > 1:
+    #     with open(sys.argv[1], 'r') as file:
+    #         formatFile(file)
+    # else:
+    #     formatFile(sys.stdin)
+    
+    parser = argparse.ArgumentParser('apache2cols.py', description="Splits an Apache log into tabular columns.")
+    # parser.add_argument('--file', required=False, type=str, help="The Apache file to process.")
+    # parser.add_argument('file', nargs="?", type=str, help="The Apache file to process.")
+    
+    # args = parser.parse_args()
+    
+    # if args.file is None:
+    #     formatFile(sys.stdin)
+    # else:
+    #     # with open(args.file, 'r') as file:
+    #     #     formatFile(file)
+    #     formatFile(open(args.file))
+    
+    parser.add_argument('file', type=argparse.FileType('r'), help="The Apache file to process.")
+    args = parser.parse_args()
+    formatFile(args.file)
     
     
-    
+if __name__ == '__main__':
+    main()
     
     
---
+++ scripts/count_bots.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+import argparse
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL) 
+
+def countBots(logData):
+    botCounts = defaultdict(int)
+    for row in logData:
+        if 'bot' in row[-1].lower():
+            botCounts[row[-1]] += 1
+            
+    return botCounts
+
+def main():
+    parser = argparse.ArgumentParser('count_bots.py', description="Counts the number of user agents that include 'bot'.")
+    parser.add_argument('file', type=argparse.FileType('r'), help="The Apache file to process (must have tab-separated columns).")
+    args = parser.parse_args()
+    countBots(args.file)
+    
+
+if __name__ == '__main__':
+    main()
